23669a6ca66272068d2a5550fd82f5eaaa217b90
sofianeallouche <sofianeallouche@yahoo.com>
2025-07-27 20:16:29 +0200

    Réalisation de l'exercice05

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,21 @@
 def sum(a, b):
+    """ 
+    Réalise une addition
+    parameters :
+        a : (int ou float) le premier nombre
+        b : (int ou float) le deuxieme nombre
+    returns : 
+        le resultat de l'addition de a + b
+    """ 
     return a + b
- 
+    
 def subtraction(a, b):
+    """ 
+    Réaliser une soustraction
+    parameters :
+        a :(int ou float) le premier nombre
+        b :(int ou float) le deuxieme nombre
+    returns: 
+        Le resultat de la soustraction de a - b 
+    """ 
     return a - b
