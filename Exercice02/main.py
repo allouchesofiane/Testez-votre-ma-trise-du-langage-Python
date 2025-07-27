eebf3b4616b76a553026f8cc8e7992bd321c404b
sofianeallouche <sofianeallouche@yahoo.com>
2025-07-27 20:11:28 +0200

    Réalisation de l'exercice02

--- Exercice02/main.py
+++ Exercice02/main.py
@@ -15,3 +15,21 @@ students = {
          'Histoire': 78
      }
 }
+def resultats_etudiant(students):
+    """ 
+    Affiche les résultats d'un etudiant à partir d'un dictionnaire
+    affiche les résulatats ou un message d'erreur
+    """
+    # Demande à l'etudiant son nom
+    Nom_etudiant = input("Entrez le nom de l'étudiant: ")
+    # Affiche les resultats ou un message d'erreur
+    if Nom_etudiant == 'Alice' or Nom_etudiant == 'Bob' or Nom_etudiant == 'Charlie' :
+        print(f"Notes de {Nom_etudiant}")
+        for matiere, note in students[Nom_etudiant].items():
+            print(f"{matiere} : {note}")
+    else :   
+        print(f"L'etudiant {Nom_etudiant} n'existe pas dans la liste.")
+        return None
+    
+# Appelle de la fonction resultats_etudiant()
+resultats_etudiant(students)
