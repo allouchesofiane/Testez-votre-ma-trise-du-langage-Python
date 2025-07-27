940771904f52ff3c8415f3c7d1874036df54eaf8
sofianeallouche <sofianeallouche@yahoo.com>
2025-07-27 20:21:55 +0200

    Réalisation de l'exercice06

--- Exercice06/main.py
+++ Exercice06/main.py
@@ -1,5 +1,11 @@
 # Fonction calculate_average
- 
+def calculate_average(numbers):
+    """Calcule la moyenne de la liste de nombres"""
+    somme = 0
+    for number in numbers:
+        somme += number
+    moyenne = somme/len(numbers)
+    return moyenne 
 # Exemple d'utilisation de la fonction
 numbers = [10, 20, 30, 40, 50]
 average = calculate_average(numbers)
