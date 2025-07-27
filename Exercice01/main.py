0552f0ffdd3a8055fbee8309e859b6a542670f75
sofianeallouche <sofianeallouche@yahoo.com>
2025-07-27 20:08:54 +0200

    Réalisation de l'exercice01

--- Exercice01/main.py
+++ Exercice01/main.py
@@ -1,2 +1,15 @@
-name = input("Entrez votre nom : ")
+# Demander le nom à l'utilisateur 
+nom = input("Entrez votre nom : ")
+# Demander le prénom à l'utilisateur 
 age = int(input("Entrez votre âge : "))
+
+def main(nom, age):
+    """
+    Fonction qui demande le nom et l âge à l utilisateur,
+    puis retourne une phrase de présentation.
+    """
+    # Affiche le nom et l'age de l'utulisateur 
+    return f"Bonjour, je m'appelle {nom} et j'ai {age} ans"
+
+# Appelle de la fonction main()
+main(nom, age)   
