77cc0a6845af1e047259a3f230ee5fbd14f2db41
sofianeallouche <sofianeallouche@yahoo.com>
2025-07-27 20:30:51 +0200

    Réalisation de l'exercice07

--- Exercice07/main.py
+++ Exercice07/main.py
@@ -1,1 +1,11 @@
-## Écrivez votre code ici !
+
+def square(number):
+    """Calcule le carré d'un nombre"""
+    if isinstance(number,(int,float)):
+        return number * number
+    else:
+        print("Le paramètre doit être un nombre !")
+        return None
+    
+# Appelle de la fonction 
+print(square(10))
