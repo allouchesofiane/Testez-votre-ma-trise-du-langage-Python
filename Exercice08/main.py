acf5edc3dfbc4cf9ac6c7afb02bad24f9ca519d0
sofianeallouche <sofianeallouche@yahoo.com>
2025-07-27 20:32:49 +0200

    Réalisation de l'exercice08

--- Exercice08/main.py
+++ Exercice08/main.py
@@ -1,8 +1,17 @@
 def log_decorator(func):
-     pass
- 
+    """
+    Décorateur qui affiche un message avant et après 
+    l'exécution d'une fonction
+    """
+    def encapsulate():
+        print("Début de la fonction.")
+        func()
+        print("Fin de la fonction.")
+    return encapsulate
+
 @log_decorator
 def function_test():
     print("Cette fonction ne prend pas d'arguments.")
 
 function_test()
+
