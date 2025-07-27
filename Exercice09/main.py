8b1c983b47e1a79c1a0b4f7ae39528368e96d36f
sofianeallouche <sofianeallouche@yahoo.com>
2025-07-27 20:34:46 +0200

    Réalisation de l'exercice09

--- Exercice09/main.py
+++ Exercice09/main.py
@@ -1,3 +1,21 @@
+class Rectangle:
+    """
+    Représente un rectangle avec une largeur et une longueur.
+    Fournit des méthodes pour calculer l'aire et le périmètre.
+    """
+    
+    def __init__(self, width, length ):
+        self.width = width 
+        self.length = length 
+    
+    def calculate_area(self):
+        area = self.width * self.length
+        return area 
+    
+    def calculate_perimeter(self):
+        perimeter = 2 * (self.width + self.length)
+        return perimeter
+
 # Test de la classe Rectangle
 rectangle = Rectangle(5, 3) # 5:width & 3:length
 print("Largeur:", rectangle.width)
